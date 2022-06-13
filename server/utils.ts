9c1cf2595dea26ba0307c6596cda5ca7d20c9159
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2022-06-13 15:31:45 +0200

    cleanup: add comment

--- server/utils.ts
+++ server/utils.ts
@@ -50,6 +50,7 @@ export const http_request = (url: string, options: http_client_Options & { body?
 
             res.pipe(concat(data => {
                 //console.log('got the response: ' + data)
+                //if (res.statusCode !== 200) console.error("request to " + url + " failed with " + res.statusCode + " " + res.statusMessage);
                 if (res.statusCode !== 200) 
                     reject({ error: data.toString(), statusCode: res.statusCode });
                 else
