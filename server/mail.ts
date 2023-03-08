b7e0983c89e13ea0856a58f2aa9b1b1ee95359a2
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2023-03-08 20:48:14 +0100

    allow "Cc:" field after "Subject:" in mail templates

--- server/mail.ts
+++ server/mail.ts
@@ -41,6 +41,17 @@ export const sendWithTemplateFile = (templateName: string, params: {}) => {
         }
     });
 }
+
+const parse_header_and_body = (rendered: string, template_for_context?: string) => {
+    let m = rendered.match(/^Subject: *(.*)(?:\nCc: *(.*))?\n\n([^]*)/);
+    if (!m) {
+        console.error("invalid template " + (template_for_context || rendered) + ': first line must be "Subject: ..."');
+        return null
+    }
+    const [, subject, cc, body] = m
+    return { subject, cc, body }
+}
+
 type params = Dictionary<any> & Pick<nodemailer.SendMailOptions, "to"|"from"|"cc">
 export const sendWithTemplate = (template: string, params: params, templateName = "") => {
     Mustache.async_render(template, params).then(rawMsg => {
@@ -50,12 +61,14 @@ export const sendWithTemplate = (template: string, params: params, templateName 
             //console.log("with params", params);
             console.log(rawMsg);
             console.log("===========================");
-            let m = rawMsg.match(/^Subject: *(.*)\n\n([^]*)/);
+            let m = parse_header_and_body(rawMsg, templateName || template)
             if (!m) {
                 console.error("invalid template " + (templateName || template) + ': first line must be "Subject: ..."');
             } else {
-                const html = `<!DOCTYPE html><html>${m[2]}</html>` // pour éviter des rejets de type HTML_MIME_NO_HTML_TAG
-                send({ from: params['from'] || conf.mail.from, to: params['to'], cc: params['cc'], subject: m[1], html }, params.moderator);
+                const html = `<!DOCTYPE html><html>${m.body}</html>` // pour éviter des rejets de type HTML_MIME_NO_HTML_TAG
+                send({ from: params['from'] || conf.mail.from, to: params['to'], cc: params['cc'] || m.cc, subject: m.subject, html }, params.moderator);
             }
     });
 };
+
+export const for_unit_tests = { parse_header_and_body }
