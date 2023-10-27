99e01790e64523c94cf78abb216d4aa154260720
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2023-10-27 21:15:11 +0200

    fix: correctly display optional newPassword

--- app/src/attrs/PasswordAttr.vue
+++ app/src/attrs/PasswordAttr.vue
@@ -1,13 +1,13 @@
 <template>
 <div>
   <my-bootstrap-form-group name="userPassword" :opts="opts" :validity="validity" hideErrors=1>
-    <input-with-validity name="userPassword" v-model="val" type="password" autocomplete="new-password" :pattern="passwordPattern" required :validity.sync="validity.userPassword"></input-with-validity>
+    <input-with-validity name="userPassword" v-model="val" type="password" autocomplete="new-password" :pattern="passwordPattern" :required="!opts.optional" :validity.sync="validity.userPassword"></input-with-validity>
     <span class="help-block" v-if="!validity.userPassword.valid">{{opts.labels && opts.labels.custom_error_message}}</span>
     <PasswordStrength v-if="!passwordPattern" :passwd="val"></PasswordStrength>
   </my-bootstrap-form-group>
 
   <my-bootstrap-form-group name="userPassword2" :label="default_attrs_title.userPassword2" :validity="validity" hideErrors=1>
-    <input-with-validity name="userPassword2" v-model="userPassword2" type="password" autocomplete="new-password" :same-as="val" required :validity.sync="validity.userPassword2"></input-with-validity>
+    <input-with-validity name="userPassword2" v-model="userPassword2" type="password" autocomplete="new-password" :same-as="val" :required="!opts.optional" :validity.sync="validity.userPassword2"></input-with-validity>
     <span class="help-block" v-if="validity.userPassword2.patternMismatch && !validity.userPassword.patternMismatch">Les mots de passe ne sont pas identiques</span>
     <span class="attr-description" v-html="opts.description"></span>
   </my-bootstrap-form-group>
