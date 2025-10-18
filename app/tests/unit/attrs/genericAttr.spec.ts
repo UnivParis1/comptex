4190865873526902b3c2359aacb2cb87b585f276
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2025-10-18 11:29:38 +0200

    WIP: migrate to Vue3 (tests)

--- app/tests/unit/attrs/genericAttr.spec.ts
+++ app/tests/unit/attrs/genericAttr.spec.ts
@@ -1,25 +1,25 @@
 import { assert, describe, it } from 'vitest';
 import * as _ from 'lodash'
-import { mount, createLocalVue, Wrapper } from '@vue/test-utils'
-import { flushPromises } from '../test_utils';
-import AsyncComputed from 'vue-async-computed';
+import { DOMWrapper, mount } from '@vue/test-utils'
+import { collect_globals, flushPromises } from '../test_utils';
 import genericAttr from '@/attrs/genericAttr.vue';
 
-import '@/directives/validators'
+import validators from '@/directives/validators'
+import { GlobalMountOptions } from '@vue/test-utils/dist/types';
 
-const inputAttrs = (eltWrapper: Wrapper<Vue>) => _.omit(eltWrapper.attributes(), 'validity', 'class')
-const inputValue = (inputWrapper: Wrapper<Vue>) => (inputWrapper.element as HTMLInputElement).value
-const inputChecked = (inputWrapper: Wrapper<Vue>) => (inputWrapper.element as HTMLInputElement).checked
+const inputAttrs = (eltWrapper: DOMWrapper<Element>) => _.omit(eltWrapper.attributes(), 'validity', 'class')
+const inputChecked = (inputWrapper: DOMWrapper<HTMLInputElement>) => inputWrapper.element.checked
 
 const mount_test = ({ name, opts, v } : { name: string, opts: StepAttrOptionM<unknown>, v: CommonV }) => {
-    const localVue = createLocalVue()
-    localVue.use(AsyncComputed)
+    let global: GlobalMountOptions = {
+        stubs: {},
+    }
+    validators(collect_globals(global))
     return mount(genericAttr, {
-        propsData: {
-            value: v[name], real_name: name, name, opts, v, stepName: 'foo',
-        }, 
-        localVue, 
-        stubs: { 'my-bootstrap-form-group': true }
+        props: {
+            modelValue: v[name], real_name: name, name, opts, v, stepName: 'foo',
+        },
+        global,
     })
 
 }
@@ -28,8 +28,8 @@ describe('minimal', async () => {
     it('renders simple input', () => {
         const wrapper = mount_test({ name: "attr1", opts: {}, v: { attr1: "a" } })
         const inputWrapper = wrapper.find('input')
-        assert.deepEqual(inputAttrs(inputWrapper), { name: "attr1", required: "required", type: "text" })
-        assert.equal(inputValue(inputWrapper), "a")
+        assert.deepEqual(inputAttrs(inputWrapper), { name: "attr1", required: "", type: "text", value: "a" })
+        assert.equal(inputWrapper.element.value, "a")
     })
 
     it('renders radio', async () => {
@@ -37,16 +37,16 @@ describe('minimal', async () => {
         const wrapper = mount_test({ name: "attr1", opts, v: { attr1: undefined } })
         await flushPromises()
         //
-        const inputs = wrapper.findAll("input").wrappers
+        const inputs = wrapper.findAll("input")
         assert.deepEqual(inputs.map(inputAttrs), [
-            { name: "attr1", required: "required", type: "radio", value: "a" },
-            { name: "attr1", required: "required", type: "radio", value: "b" },
+            { name: "attr1", required: "", type: "radio", value: "a" },
+            { name: "attr1", required: "", type: "radio", value: "b" },
         ])
         // nothing selected by default
         assert.deepEqual(inputs.map(inputChecked), [false, false])
 
         // selecting 2nd
-        await inputs[1].setChecked()
+        await inputs[1].setValue(true)
         assert.deepEqual(inputs.map(inputChecked), [false, true])
     })
 
@@ -56,14 +56,14 @@ describe('minimal', async () => {
         await flushPromises()
         //
         const selectWrapper = wrapper.find('select')
-        assert.deepEqual(inputAttrs(selectWrapper), { name: "attr1", required: "required" })
-        const options = wrapper.findAll("select option").wrappers
+        assert.deepEqual(inputAttrs(selectWrapper), { name: "attr1", required: "", value: "a" })
+        const options = wrapper.findAll("select option")
         assert.deepEqual(options.map(inputAttrs), [ { value: "a" }, { value: "b" } ])
         // nothing selected by default
-        assert.equal(inputValue(selectWrapper), 'a')
+        assert.equal(selectWrapper.element.value, 'a')
 
-        await options[1].setSelected()
-        assert.equal(inputValue(selectWrapper), 'b')
+        await options[1].setValue('selected')
+        assert.equal(selectWrapper.element.value, 'b')
     })
 
     it('renders select with invalid choice', async () => {
@@ -72,14 +72,14 @@ describe('minimal', async () => {
         await flushPromises()
         //
         const selectWrapper = wrapper.find('select')
-        assert.deepEqual(inputAttrs(selectWrapper), { name: "attr1", required: "required" })
-        const options = wrapper.findAll("select option").wrappers
-        assert.deepEqual(options.map(inputAttrs), [ { disabled: "disabled", hidden: "hidden", value: "" },{ value: "a" }, { value: "b" } ])
+        assert.deepEqual(inputAttrs(selectWrapper), { name: "attr1", required: "" })
+        const options = wrapper.findAll("select option")
+        assert.deepEqual(options.map(inputAttrs), [ { disabled: "", hidden: "" },{ value: "a" }, { value: "b" } ])
         // nothing selected by default
-        assert.equal(inputValue(selectWrapper), '')
+        assert.equal(selectWrapper.element.value, '')
 
-        await options[2].setSelected()
-        assert.equal(inputValue(selectWrapper), 'b')
+        await options[2].setValue('selected')
+        assert.equal(selectWrapper.element.value, 'b')
     })
 
     it('renders select with optional value', async () => {
@@ -89,13 +89,13 @@ describe('minimal', async () => {
         //
         const selectWrapper = wrapper.find('select')
         assert.deepEqual(inputAttrs(selectWrapper), { name: "attr1" })
-        const options = wrapper.findAll("select option").wrappers
+        const options = wrapper.findAll("select option")
         assert.deepEqual(options.map(inputAttrs), [ { value: "" }, { value: "a" }, { value: "b" } ])
         // nothing selected by default
-        assert.equal(inputValue(selectWrapper), '')
+        assert.equal(selectWrapper.element.value, '')
 
-        await options[2].setSelected()
-        assert.equal(inputValue(selectWrapper), 'b')
+        await options[2].setValue('selected')
+        assert.equal(selectWrapper.element.value, 'b')
     })
 
     it('limits oneOf to opts.max', async () => {
@@ -106,7 +106,7 @@ describe('minimal', async () => {
         //
         assert.deepEqual(vm.oneOf, [ { const: "1", title: "A" }, { const: "2", title: "B" } ])
         // if max changes, choices change:
-        opts.max = 1
+        vm.opts.max = 1
         assert.deepEqual(vm.oneOf, [ { const: "1", title: "A" } ])
     })
     
