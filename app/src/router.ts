7c8051d73358b06c617f5887055d1c3ef7ba1993
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2025-10-18 11:29:02 +0200

    WIP: migrate to Vue3 (router)

--- app/src/router.ts
+++ app/src/router.ts
@@ -1,14 +1,15 @@
-import VueRouter from "vue-router";
+import { createRouter, createWebHistory, RouterOptions, RouteRecordRaw, Router } from 'vue-router'
 import conf from './conf';
 import * as Helpers from './services/helpers'; 
 import Step from './controllers/Step.vue';
 import ModerateList from './controllers/ModerateList.vue';
 
 import template_welcome from './templates/welcome.html?raw'
+import { Component } from 'vue';
 
-export let router;
+export let router: Router;
 
-const _routes = {
+const _routes: Dictionary<Component> = {
     '/playground': () => import('./controllers/Playground.vue'),
     '/login/:kind?': { render(_h) { router.replace(this.$route.query.then) } }, // TODO, use vue-router redirect
     '/steps/:kind?': ModerateList,
@@ -16,22 +17,16 @@ const _routes = {
     '/': { template: template_welcome },
 };
 
-const routes = [];
+const routes: RouteRecordRaw[] = [];
 Helpers.eachObject(_routes, (path, component) => {
     routes.push({ path, component, props: true })
 });
 
 if (!conf.base_pathname.match(/\/$/)) alert("base_pathname in vue.config.js must have a trailing slash");
 
-const opts = {
-    mode: undefined,
-    base: conf.base_pathname,
+const opts: RouterOptions = {
+    history: createWebHistory(conf.base_pathname),
     routes,
 };
 
-declare var process;
-if (process.env.NODE_ENV === 'production') {
-    opts.mode = 'history';
-}
-
-router = new VueRouter(opts);
+router = createRouter(opts);
